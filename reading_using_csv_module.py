52695746749b0881ca2b66d6b408ea52803f0307
kiran-kadam-2021 <kirankadam633@gmail.com>
2023-08-15 16:26:44 +0530

    Added script to read text/csv files

---
+++ reading_using_csv_module.py
@@ -0,0 +1,10 @@
+import csv
+with open("Data/people-100.csv", "r") as file:
+    lines = csv.reader(file)
+    num = 0
+    for line in lines:
+        print(line)
+        num += 1
+        if num == 5:
+            break
+    
